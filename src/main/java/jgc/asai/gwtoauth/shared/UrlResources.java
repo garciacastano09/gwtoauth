d79b055a8288f523d862688dd7e300a749b21ad1
Jorge García Castaño <garciacastano09@gmail.com>
2018-05-26 19:55:40 +0200

    linkedin more params in wury

--- src/main/java/jgc/asai/gwtoauth/shared/UrlResources.java
+++ src/main/java/jgc/asai/gwtoauth/shared/UrlResources.java
@@ -3,7 +3,7 @@ package jgc.asai.gwtoauth.shared;
 public class UrlResources {
     public static final String GOOGLE_PLUS_ME = "https://www.googleapis.com/plus/v1/people/me";
     public static final String GOOGLE_DRIVE_FILES = "https://www.googleapis.com/drive/v3/files";
-    public static final String LINKEDIN_ME = "https://api.linkedin.com/v1/people/~:(first-name,last-name,email-address,summary)?format=json";
+    public static final String LINKEDIN_ME = "https://api.linkedin.com/v1/people/~:(first-name,last-name,email-address,industry,positions,picture-url,summary)?format=json";
 
     public static final String CALLBACK_URL = "http://127.0.0.1:8888/BaseApp.html";
 }
